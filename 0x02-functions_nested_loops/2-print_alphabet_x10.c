89f4e47d0ed7a9ee9af6ea3e4ac8f9f41ca46f12
WAKAM Hermann <wakam.hermann@gmail.com>
2022-07-05 10:21:56 -0700

    task2-20220705-1821

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char x;
+	x = 0;
+
+	while (x < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
