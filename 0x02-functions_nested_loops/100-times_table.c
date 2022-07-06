3665228042e13d6b9ee9d2798f4a01006439631b
WAKAM Hermann <wakam.hermann@gmail.com>
2022-07-05 17:55:27 -0700

    task100-20220706-0155

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,8 +13,10 @@ void print_times_table(int n)
 		{
 			z = x * y;
 			if (z == 0 && y == 0)
+			{
 				_putchar(z + '0');
-			else if (z > 99)
+			} else if (z > 99)
+			{
 				k = z % 10;
 				t = (z / 10) % 10;
 				z /= 100;
@@ -23,7 +25,8 @@ void print_times_table(int n)
 				_putchar(z + '0');
 				_putchar(t + '0');
 				_putchar(k + '0');
-			else if (z > 9)
+			} else if (z > 9)
+			{
 				k = z % 10;
 				z /= 10;
 				_putchar(',');
@@ -31,12 +34,14 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(z + '0');
 				_putchar(k + '0');
-			else
+			} else
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(z + '0');
+			}
 		}
 		_putchar('\n');
 	}
