bac4994daeae3a34d4b45656e8f28d32db722380
WAKAM Hermann <wakam.hermann@gmail.com>
2022-07-07 05:26:58 -0700

    morefct567-20220707-1326

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ * @n: size of line
+ * Return: void
+ */
+
+int print_line(int n)
+{
+	for (; n > 0; i--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print straight line
+ * @n: size of line
+ * Return: void
+ */
+
+int print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (y = 0; y < n; y++)
+	{
+		for (x = y; x > 0; x--)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
