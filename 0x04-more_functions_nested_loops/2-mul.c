4c627e91903b9fa18a81d43efe3e5d1bacc532d0
WAKAM Hermann <wakam.hermann@gmail.com>
2022-07-07 04:25:00 -0700

    morefct01234-20220707-1224

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: value of the 02 multiplied integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
