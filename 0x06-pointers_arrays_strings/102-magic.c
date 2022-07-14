117f8f8d3b651440317678661fa660008ed8dac0
WAKAM Hermann <wakam.hermann@gmail.com>
2022-07-14 07:51:32 -0700

    tasks102-20220714-1549

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - entry point
- * Return: Always 0
- */
 
 int main(void)
 {
@@ -22,6 +18,6 @@ int main(void)
 	*/
 	p[5] = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d \n", a[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
