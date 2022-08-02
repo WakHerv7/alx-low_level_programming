d594def8bf4bc08c7d59d4c8ae7f922f85b4eceb
WAKAM Hermann <wakam.hermann@gmail.com>
2022-08-01 17:30:47 -0700

    Task4-0x0E-20220802-0130

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,13 @@
 **/
 int _strlen(char *s)
 {
-int i;
-for (i = 0; *s != '\0'; s++)
-{
-i++;
-}
-return (i);
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -32,15 +33,15 @@ return (i);
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-for (i = i; i < n; i++)
-dest[i] = '\0';
+	for (i = i; i < n; i++)
+		dest[i] = '\0';
 
-return (dest);
+	return (dest);
 }
 
 /**
@@ -55,37 +56,38 @@ return (dest);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *newDog;
-char *newName;
-char *newOwner;
-int size = 4;
+	dog_t *newDog;
 
-newDog = (dog_t *)malloc(sizeof(struct dog) * size);
-if (newDog == NULL)
-{
-free(newDog);
-return (NULL);
-}
-newName = (char *)malloc(sizeof((name) + 1) * size);
-if (newName == NULL)
-{
-free(newDog);
-return (NULL);
-}
-newOwner = (char *)malloc(sizeof((owner) + 1) * size);
-if (newOwner == NULL)
-{
-free(newDog);
-free(newName);
-return (NULL);
-}
-newName = name;
-newOwner = owner;
+	char *newName;
+	char *newOwner;
+	int size = 4;
 
-newDog->name = newName;
-newDog->owner = newOwner;
-newDog->age = age;
+	newDog = (dog_t *)malloc(sizeof(struct dog) * size);
+	if (newDog == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	newName = (char *)malloc(sizeof((name) + 1) * size);
+	if (newName == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	newOwner = (char *)malloc(sizeof((owner) + 1) * size);
+	if (newOwner == NULL)
+	{
+		free(newDog);
+		free(newName);
+		return (NULL);
+	}
+	newName = name;
+	newOwner = owner;
 
-return (newDog);
+	newDog->name = newName;
+	newDog->owner = newOwner;
+	newDog->age = age;
+
+	return (newDog);
 }
 
