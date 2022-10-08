da10180cf26a07c50741cf8ceeec5ed9d77b06f6
Wakam Kamdem Vanel <wakam.hermann@gmail.com>
2022-10-07 20:25:31 -0700

    wakherv-005

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int _add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return(c);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int _mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return(c);
+}
